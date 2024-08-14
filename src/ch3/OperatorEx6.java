f06ede234c02d681a70afad425ea4219cca688eb
bkw535 <bkw54570@gmail.com>
2024-08-14 14:51:28 +0900

    ch3

---
+++ src/ch3/OperatorEx6.java
@@ -0,0 +1,10 @@
+package ch3;
+
+public class OperatorEx6 {
+    public static void main(String[] args) {
+        byte a=10;
+        byte b=30;
+        byte c=(byte)(a*b); // byte명시 필요
+        System.out.println(c);
+    }
+}
---
+++ src/ch3/OperatorEx7.java
@@ -0,0 +1,10 @@
+package ch3;
+
+public class OperatorEx7 {
+    public static void main(String[] args) {
+        long a = 2_000_000 * 2_000_000;
+        long b = 2_000_000 * 2_000_000L;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
